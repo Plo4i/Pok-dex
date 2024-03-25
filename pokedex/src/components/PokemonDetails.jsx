04bf470accaffcdcbf7ca13a58b35411ba4fb84c
Plo4i <midag@abv.bg>
2024-03-25 17:43:47 +0200

    Added about info: Should be styled in the future!

--- pokedex/src/components/PokemonDetails.jsx
+++ pokedex/src/components/PokemonDetails.jsx
@@ -18,7 +18,38 @@ const PokemonDetails = () => {
  return (
     <div>
       <h2 className='pokemon-name'>{pokemonDetails.name}</h2>
-      {/* Display other details here */}
+      <div>
+        <h3>Types:</h3>
+        <ul>
+          {pokemonDetails.types && pokemonDetails.types.map((type, index) => (
+            <li key={index}>{type.type.name}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h3>Stats:</h3>
+        <ul>
+          {pokemonDetails.stats && pokemonDetails.stats.map((stat, index) => (
+            <li key={index}>{stat.stat.name}: {stat.base_stat}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h3>Abilities:</h3>
+        <ul>
+          {pokemonDetails.abilities && pokemonDetails.abilities.map((ability, index) => (
+            <li key={index}>{ability.ability.name}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h3>Height:</h3>
+        <p>{pokemonDetails.height / 10} m</p>
+      </div>
+      <div>
+        <h3>Weight:</h3>
+        <p>{pokemonDetails.weight / 10} kg</p>
+      </div>
     </div>
  );
 };
