86fb9cb359a10287daa11cb12f3964c6071a643f
Plo4i <midag@abv.bg>
2024-04-11 17:21:20 +0200

    Try to use hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import PokemonList from './components/PokemonList';
 import PokemonDetails from './components/PokemonDetails';
 import About from './pages/About';
